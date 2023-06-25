a7fb4a7d919d37c27aa885a05fe60cf2829c0806
Oded Regev <odedre@gmail.com>
2023-06-25 14:28:59 +0300

    small fixes to UI layout

--- layouts/SlideLayout.js
+++ layouts/SlideLayout.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export const SlideLayout = ({ title, direction = 'row', children }) => {
-    return (<div style={{ width: '100vw', height: '100vh' }}>
-        <div style={{height: '10%'}}>
+    return (<div style={{ width: '80vw', height: '100vh' }}>
+        <div>
             <h1 style={{textAlign: 'center'}}>{title}</h1>
         </div>
         <div style={{display: 'flex', justifyContent: 'center', flexDirection: direction }}>
